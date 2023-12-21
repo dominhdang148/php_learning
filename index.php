11078ce1016f2e3a9ee27f86932bdf0e0102634b
dominhdang148 <myclone235@gmail.com>
2023-12-21 22:55:06 +0700

    finish syntax

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-    echo 'Hello World';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>
+       <?= 'Hello World'?>
+    </h1>
+
+    <p>My first paragraph</p>
+     <?php
+            $x=5;
+            $y=10;   
+            echo '<p>'.$x.','.$y. '</p>';
+        ?> 
+        
+</body>
+</html>
